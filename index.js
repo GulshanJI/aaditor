1587672dace7778608a216c81dbfa4d740ef9a6a
Gulshan JI <mr.gulshan.attri@gmail.com>
2019-11-27 07:41:08 -0500

    Remove Unnecessary console.log call

---
+++ index.js
@@ -0,0 +1,22 @@
+const path = require('path')
+const express = require('express')
+const app = express()
+const hbs = require('hbs')
+const webPage = path.join(__dirname, './webpage')
+const partialPath = path.join(__dirname, './partial')
+app.set('view engine', 'hbs')
+app.set('views', webPage)
+
+hbs.registerPartials(partialPath)
+
+app.get('', (req, res) => {
+    res.render('index', {
+        pagename: req.query.urname,
+        myname: req.query.pagename
+    })
+})
+
+
+app.listen(3000, () => {
+    console.log("Server is up on port 3000")
+})
